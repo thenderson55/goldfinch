37a66773a72ca75043a11b17ae01867e7414bef1
Thomas Henderson <tbhenderson55@yahoo.com>
2021-11-22 17:49:11 +0000

    img/env working with next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+  env: {
+    customKey: 'my-value',
+  },
+  images: {
+      domains: ["rickandmortyapi.com"],
+  },
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
+import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import Image from "next/Image";
+import { Character, GetCharacterResults } from "../types";
 import styles from "./index.module.scss";
 
-const Home = () => (
+const Home: NextPage<{ characters: Character[] }> = ({ characters }) => (
   <div className={styles.container}>
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-
+    <h1>The value of customKey is: {process.env.customKey}</h1>
+    {characters.map((character) => {
+      return (
+        <div key={character.id}>
+          <li>{character.name}</li>;
+          <Image
+            src={character.image}
+            alt={character.name}
+            width="200"
+            height="200"
+          />
+        </div>
+      );
+    })}
     <main>
       <h1 className={styles.title}>
         Welcome <a href="https://nextjs.org">Next.js!</a>
@@ -44,5 +60,16 @@ const Home = () => (
     </main>
   </div>
 );
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await fetch("https://rickandmortyapi.com/api/character");
+  const { results }: GetCharacterResults = await res.json();
+
+  return {
+    props: {
+      characters: results,
+    },
+  };
+};
 
 export default Home;
